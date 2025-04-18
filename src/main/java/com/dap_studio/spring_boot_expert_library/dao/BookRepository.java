90a496898938c7c44daa7e266d1f5a60185a5211
Borzoi <siteandstock@gmail.com>
2025-04-18 21:42:03 +0200

    [6] Add a method to search books by title with pagination
    Introduced a `findByTitleContaining` method in `BookRepository` to support searching books by title. This method uses pagination via the `Pageable` parameter to handle large datasets efficiently.

--- src/main/java/com/dap_studio/spring_boot_expert_library/dao/BookRepository.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/dao/BookRepository.java
@@ -1,7 +1,12 @@
 package com.dap_studio.spring_boot_expert_library.dao;
 
 import com.dap_studio.spring_boot_expert_library.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
