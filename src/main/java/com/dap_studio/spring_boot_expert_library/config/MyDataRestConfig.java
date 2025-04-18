98a0b5ed22e16c3c0f261df463b5889a2a9b0585
Borzoi <siteandstock@gmail.com>
2025-04-18 21:40:08 +0200

    [5] Add data REST configuration and update Lombok dependency scope
    Implemented a custom `RepositoryRestConfigurer` to expose IDs, disable certain HTTP methods, and configure CORS for the `Book` entity. Updated the Lombok dependency scope in `pom.xml` to `provided` and removed redundant Maven compiler plugin configuration.

---
+++ src/main/java/com/dap_studio/spring_boot_expert_library/config/MyDataRestConfig.java
@@ -0,0 +1,45 @@
+package com.dap_studio.spring_boot_expert_library.config;
+
+import com.dap_studio.spring_boot_expert_library.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final String theAllowedOrigins = "http://localhost:5173";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                                                     CorsRegistry cors) {
+        HttpMethod[] theUnsupportedMethods = {
+                POST,
+                PATCH,
+                PUT,
+                DELETE
+        };
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, theUnsupportedMethods);
+
+        /* Configure CORS Mapping */
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class<Book> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedMethods) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedMethods))
+                .withCollectionExposure((metadata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedMethods));
+    }
+
+}
