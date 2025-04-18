278b4b1eb6b62f1da1b41123c61fe1e68af3cb43
Borzoi <siteandstock@gmail.com>
2025-04-18 22:08:22 +0200

    [9] Add support for Review entity in REST configuration
    Expose IDs and disable unsupported HTTP methods for the Review entity. Introduce a `ReviewRepository` interface to handle Review persistence. Adjust the type parameter in the ` disableHttpMethods ` method for flexibility.

--- src/main/java/com/dap_studio/spring_boot_expert_library/config/MyDataRestConfig.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.dap_studio.spring_boot_expert_library.config;
 
 import com.dap_studio.spring_boot_expert_library.entity.Book;
+import com.dap_studio.spring_boot_expert_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -25,15 +26,17 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         };
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedMethods);
+        disableHttpMethods(Review.class, config, theUnsupportedMethods);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
                 .allowedOrigins(theAllowedOrigins);
     }
 
-    private void disableHttpMethods(Class<Book> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedMethods) {
+    private void disableHttpMethods(Class<?> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedMethods) {
         config.getExposureConfiguration()
                 .forDomainType(theClass)
                 .withItemExposure((metadata, httpMethods) ->
