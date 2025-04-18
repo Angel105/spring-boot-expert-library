fe5beb2458f212b0f5db2f8eb83de47c3777f81f
Borzoi <siteandstock@gmail.com>
2025-04-18 20:45:05 +0200

    [0] Initial commit

---
+++ src/main/java/com/dap_studio/spring_boot_expert_library/SpringBootExpertLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.dap_studio.spring_boot_expert_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpertLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpertLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dap_studio/spring_boot_expert_library/SpringBootExpertLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dap_studio.spring_boot_expert_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpertLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
