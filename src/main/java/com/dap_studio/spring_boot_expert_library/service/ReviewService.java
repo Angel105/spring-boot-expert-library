99af13f62a4bc261b7a0bb03b6115b8b36d946a3
Borzoi <siteandstock@gmail.com>
2025-04-26 18:55:57 +0200

    [24] Add ReviewService to handle posting book reviews
    Introduced a new service class, `ReviewService`, to manage review-related operations. This includes functionality to post reviews while ensuring that duplicate reviews for the same book by the same user are not allowed. The service integrates with `BookRepository` and `ReviewRepository` for database interactions.

---
+++ src/main/java/com/dap_studio/spring_boot_expert_library/service/ReviewService.java
@@ -0,0 +1,45 @@
+package com.dap_studio.spring_boot_expert_library.service;
+
+import com.dap_studio.spring_boot_expert_library.dao.BookRepository;
+import com.dap_studio.spring_boot_expert_library.dao.ReviewRepository;
+import com.dap_studio.spring_boot_expert_library.entity.Review;
+import com.dap_studio.spring_boot_expert_library.request_models.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+@Transactional
+public class ReviewService {
+
+    private final BookRepository bookRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(BookRepository bookRepository, ReviewRepository reviewRepository) {
+        this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, reviewRequest.getBookId());
+        if (validateReview != null) {
+            throw new Exception("Review already created for this book");
+        }
+
+        Review review = new Review();
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(reviewRequest.getRating());
+        review.setUserEmail(userEmail);
+        if (reviewRequest.getReviewDescription().isPresent()) {
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(
+                    Object::toString
+            ).orElse(null));
+        }
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+    }
+}
