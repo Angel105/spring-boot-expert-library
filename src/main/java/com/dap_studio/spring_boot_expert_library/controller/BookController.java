2e306cf73b5fc67897f16d1dd56a4e8d02965771
Borzoi <siteandstock@gmail.com>
2025-05-03 19:31:12 +0200

    [30] Add endpoint to fetch user's current loan details
    Introduced a new endpoint `/secure/currentLoans` in `BookController` to fetch the list of current loans for a user. The endpoint extracts the user's email from the JWT token and retrieves the corresponding data from the `BookService`.

--- src/main/java/com/dap_studio/spring_boot_expert_library/controller/BookController.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.dap_studio.spring_boot_expert_library.controller;
 
 import com.dap_studio.spring_boot_expert_library.entity.Book;
+import com.dap_studio.spring_boot_expert_library.response_models.ShelfCurrentLoansResponse;
 import com.dap_studio.spring_boot_expert_library.service.BookService;
 import com.dap_studio.spring_boot_expert_library.utils.ExtractJWT;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:5173")
 @RestController
@@ -16,6 +19,12 @@ public class BookController {
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping("/secure/currentLoans")
+    public List<ShelfCurrentLoansResponse> currentLoans(@RequestHeader(value = "Authorization") String token) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoans(userEmail);
     }
 
     @GetMapping("/secure/currentLoans/count")
