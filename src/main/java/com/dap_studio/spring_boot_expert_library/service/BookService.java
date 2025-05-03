55bb020111b5578006803d2cb335c6281d91e957
Borzoi <siteandstock@gmail.com>
2025-05-03 17:58:08 +0200

    [29] Add a method to fetch current loans with remaining days
    Introduced `currentLoans` method in `BookService` to calculate and return books currently loaned by a user with the number of remaining days until their return. Added a custom query in `BookRepository` to fetch books by a list of IDs to support this functionality.

--- src/main/java/com/dap_studio/spring_boot_expert_library/service/BookService.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/service/BookService.java
@@ -4,11 +4,17 @@ import com.dap_studio.spring_boot_expert_library.dao.BookRepository;
 import com.dap_studio.spring_boot_expert_library.dao.CheckoutRepository;
 import com.dap_studio.spring_boot_expert_library.entity.Book;
 import com.dap_studio.spring_boot_expert_library.entity.Checkout;
+import com.dap_studio.spring_boot_expert_library.response_models.ShelfCurrentLoansResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
@@ -56,5 +62,39 @@ public class BookService {
 
     public int currentLoansCount(String userEmail) {
         return checkoutRepository.findBooksByUserEmail(userEmail).size();
+    }
+
+    public List<ShelfCurrentLoansResponse> currentLoans(String userEmail) throws Exception {
+        List<ShelfCurrentLoansResponse> shelfCurrentLoansResponses = new ArrayList<>();
+
+        List<Checkout> checkoutList = checkoutRepository.findBooksByUserEmail(userEmail);
+        
+        List<Long> bookIdList = new ArrayList<>();
+
+        for (Checkout checkout : checkoutList) {
+            bookIdList.add(checkout.getBookId());
+        }
+
+//        List<Book> bookList = bookRepository.findAllById(bookIdList);
+        List<Book> bookList = bookRepository.findBooksByBookIds(bookIdList);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        for (Book book : bookList) {
+            Optional<Checkout> checkout = checkoutList.stream()
+                    .filter(x -> x.getBookId() == book.getId()).findFirst();
+
+            if (checkout.isPresent()) {
+                Date d1 = sdf.parse(checkout.get().getReturnDate());
+                Date d2 = sdf.parse(LocalDate.now().toString());
+
+                TimeUnit time = TimeUnit.DAYS;
+                long diffInDays = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+
+                shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) diffInDays));
+            }
+        }
+
+        return shelfCurrentLoansResponses;
     }
 }
