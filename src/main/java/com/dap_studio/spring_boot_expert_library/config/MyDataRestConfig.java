d435b36983ed5863b5bb0887eeaf70fe21ccbca6
Borzoi <siteandstock@gmail.com>
2025-05-14 19:56:11 +0200

    [39] Expose History entity IDs and fix method parameter in the repository
    Added configuration to expose IDs for the History entity in `MyDataRestConfig` to ensure proper functionality with REST endpoints. Additionally, corrected the parameter name in `HistoryRepository` from "email" to "userEmail" for consistency and accuracy.

--- src/main/java/com/dap_studio/spring_boot_expert_library/config/MyDataRestConfig.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.dap_studio.spring_boot_expert_library.config;
 
 import com.dap_studio.spring_boot_expert_library.entity.Book;
+import com.dap_studio.spring_boot_expert_library.entity.History;
 import com.dap_studio.spring_boot_expert_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -27,6 +28,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
+        config.exposeIdsFor(History.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedMethods);
         disableHttpMethods(Review.class, config, theUnsupportedMethods);
