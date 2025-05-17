7e2798ac7705c0badfd77269b6f62c64da5c7b18
Borzoi <siteandstock@gmail.com>
2025-05-17 19:12:23 +0200

    [46] Add Message entity support and custom query to the repository
    The Message entity is now exposed in the API and its HTTP methods are configured. A custom query method, `findByUserEmail`, was added to the `MessageRepository` to allow paginated searches by user email.

--- src/main/java/com/dap_studio/spring_boot_expert_library/config/MyDataRestConfig.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/config/MyDataRestConfig.java
@@ -2,6 +2,7 @@ package com.dap_studio.spring_boot_expert_library.config;
 
 import com.dap_studio.spring_boot_expert_library.entity.Book;
 import com.dap_studio.spring_boot_expert_library.entity.History;
+import com.dap_studio.spring_boot_expert_library.entity.Message;
 import com.dap_studio.spring_boot_expert_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -29,10 +30,12 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
         config.exposeIdsFor(History.class);
+        config.exposeIdsFor(Message.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedMethods);
         disableHttpMethods(Review.class, config, theUnsupportedMethods);
         disableHttpMethods(History.class, config, theUnsupportedMethods);
+        disableHttpMethods(Message.class, config, theUnsupportedMethods);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
