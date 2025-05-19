1a10215483ef4569646bea8b85508ba447c06559
Borzoi <siteandstock@gmail.com>
2025-05-19 22:39:21 +0200

    [50] Add an endpoint to update messages for admins
    Introduced a PUT endpoint `/secure/admin/message` that allows administrators to update messages. The endpoint validates the user's admin status and processes the update request using the `MessagesService`.

--- src/main/java/com/dap_studio/spring_boot_expert_library/controller/MessagesController.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.dap_studio.spring_boot_expert_library.controller;
 
 import com.dap_studio.spring_boot_expert_library.entity.Message;
+import com.dap_studio.spring_boot_expert_library.request_models.AdminQuestionRequest;
 import com.dap_studio.spring_boot_expert_library.service.MessagesService;
 import com.dap_studio.spring_boot_expert_library.utils.ExtractJWT;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +22,16 @@ public class MessagesController {
                             @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value="Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
