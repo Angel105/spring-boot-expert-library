b64e5591974338cef55f653168edb90d813644be
Borzoi <siteandstock@gmail.com>
2025-05-19 22:26:48 +0200

    [48] Add AdminQuestionRequest model
    Introduce the AdminQuestionRequest class with fields for id and response. This serves as a data model for handling admin question requests.

---
+++ src/main/java/com/dap_studio/spring_boot_expert_library/request_models/AdminQuestionRequest.java
@@ -0,0 +1,11 @@
+package com.dap_studio.spring_boot_expert_library.request_models;
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+}
