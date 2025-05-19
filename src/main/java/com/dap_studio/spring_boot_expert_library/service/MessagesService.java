826fc3d50504484213ea9334db37127804acdd41
Borzoi <siteandstock@gmail.com>
2025-05-19 22:32:35 +0200

    [49] Add a method to handle admin responses to messages
    Introduce a `putMessage` method in `MessagesService` to process admin responses. The method updates the message with an admin email, response, and marks it as closed. It also handles cases where the message ID does not exist by throwing an exception.

--- src/main/java/com/dap_studio/spring_boot_expert_library/service/MessagesService.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/service/MessagesService.java
@@ -2,9 +2,11 @@ package com.dap_studio.spring_boot_expert_library.service;
 
 import com.dap_studio.spring_boot_expert_library.dao.MessageRepository;
 import com.dap_studio.spring_boot_expert_library.entity.Message;
+import com.dap_studio.spring_boot_expert_library.request_models.AdminQuestionRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -20,5 +22,16 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messagesRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messagesRepository.findById(adminQuestionRequest.getId());
+        if (!message.isPresent()) {
+            throw new Exception("Message not found");
+        }
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messagesRepository.save(message.get());
     }
 }
