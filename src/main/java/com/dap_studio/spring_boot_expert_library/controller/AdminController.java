f1bb96f0d6f4aed0ed0933fabfac1c01db90c7ff
Borzoi <siteandstock@gmail.com>
2025-05-20 17:40:27 +0200

    [52] Add admin book management with secure endpoints
    Introduced `AdminService` and `AdminController` to enable adding new books via a secure `/api/admin/secure/add/book` endpoint. Expanded security configuration to protect admin endpoints, ensuring only users with the appropriate "admin" role can access them.

---
+++ src/main/java/com/dap_studio/spring_boot_expert_library/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.dap_studio.spring_boot_expert_library.controller;
+
+import com.dap_studio.spring_boot_expert_library.request_models.AddBookRequest;
+import com.dap_studio.spring_boot_expert_library.service.AdminService;
+import com.dap_studio.spring_boot_expert_library.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value= "Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws  Exception {
+
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        adminService.postBook(addBookRequest);
+    }
+}
