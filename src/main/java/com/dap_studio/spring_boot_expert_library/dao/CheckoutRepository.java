9b941ccd02e08737a5c5d3cd908302ac1f858af5
Borzoi <siteandstock@gmail.com>
2025-05-21 22:23:19 +0200

    [57] Add bulk delete methods for Checkout and Review repositories
    Introduced `deleteAllByBookId` methods in both `CheckoutRepository` and `ReviewRepository` to support bulk deletion of records by `bookId`. This includes the use of `@Modifying`, `@Query`, and parameter binding for efficient database operations.

--- src/main/java/com/dap_studio/spring_boot_expert_library/dao/CheckoutRepository.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/dao/CheckoutRepository.java
@@ -2,6 +2,9 @@ package com.dap_studio.spring_boot_expert_library.dao;
 
 import com.dap_studio.spring_boot_expert_library.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,9 @@ public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
 
     List<Checkout> findBooksByUserEmail(String userEmail);
+
+    @Modifying
+    @Query("delete from Checkout o where o.bookId = :book_id")
+//    @Query("delete from Checkout where bookId in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
