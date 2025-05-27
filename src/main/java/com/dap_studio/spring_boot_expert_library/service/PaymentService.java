d0665ed8e6ec928aa7b6b4e82c39bc39717d921e
Borzoi <siteandstock@gmail.com>
2025-05-27 18:24:12 +0200

    [67] Add `stripePayment` method for handling user payments
    Introduce a new `stripePayment` method in PaymentService to retrieve user payment details, update the amount, and save it. This method ensures proper handling of missing payment information and returns an appropriate HTTP status.

--- src/main/java/com/dap_studio/spring_boot_expert_library/service/PaymentService.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/service/PaymentService.java
@@ -1,11 +1,14 @@
 package com.dap_studio.spring_boot_expert_library.service;
 
 import com.dap_studio.spring_boot_expert_library.dao.PaymentRepository;
+import com.dap_studio.spring_boot_expert_library.entity.Payment;
 import com.dap_studio.spring_boot_expert_library.request_models.PaymentInfoRequest;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,6 +38,16 @@ public class PaymentService {
         params.put("payment_method_types", paymentMethodTypes);
 
         return PaymentIntent.create(params);
+    }
+
+    public ResponseEntity<String> stripePayment(String userEmail) throws Exception {
+        Payment payment = paymentRepository.findByUserEmail(userEmail);
+        if (payment == null) {
+            throw new Exception("Payment information is missing");
+        }
+        payment.setAmount(00.00);
+        paymentRepository.save(payment);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
