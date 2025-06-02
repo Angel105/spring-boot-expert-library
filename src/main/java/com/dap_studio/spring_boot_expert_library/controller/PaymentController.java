b1809fb9f41e1174238086e894fc704c1ed4f484
Borzoi <siteandstock@gmail.com>
2025-06-02 14:17:49 +0200

    [69] Add endpoints for payment intent and payment completion
    Implemented `/payment-intent` for creating payment intents and `/payment-complete` for completing payments with Stripe integration. Added necessary request models, utility methods, and error handling for token-based authentication.

--- src/main/java/com/dap_studio/spring_boot_expert_library/controller/PaymentController.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/controller/PaymentController.java
@@ -1,9 +1,13 @@
 package com.dap_studio.spring_boot_expert_library.controller;
 
+import com.dap_studio.spring_boot_expert_library.request_models.PaymentInfoRequest;
 import com.dap_studio.spring_boot_expert_library.service.PaymentService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dap_studio.spring_boot_expert_library.utils.ExtractJWT;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("https://localhost:5173")
 @RestController
@@ -14,5 +18,29 @@ public class PaymentController {
 
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoRequest paymentInfoRequest)
+            throws StripeException {
+
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInfoRequest);
+        String paymentString = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentString, HttpStatus.OK);
+    }
+
+    @PutMapping("/payment-complete")
+    public ResponseEntity<String> stripePaymentComplete(@RequestHeader(value= "Authorization") String token)
+        throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing from token");
+        }
+        ResponseEntity<String> response = paymentService.stripePayment(userEmail);
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new Exception("Stripe payment failed");
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
