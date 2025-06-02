b2bd4087606fcc759bc249b10041c374c0682269
Borzoi <siteandstock@gmail.com>
2025-06-02 14:31:28 +0200

    [70] Add payment validation and outstanding fee check to BookService
    Integrated PaymentRepository into BookService to manage user payments. Added logic to check for outstanding fees or overdue books before allowing a user to checkout new books. Also ensured a Payment record is initialized for new users if none exists.

--- src/main/java/com/dap_studio/spring_boot_expert_library/service/BookService.java
+++ src/main/java/com/dap_studio/spring_boot_expert_library/service/BookService.java
@@ -3,9 +3,11 @@ package com.dap_studio.spring_boot_expert_library.service;
 import com.dap_studio.spring_boot_expert_library.dao.BookRepository;
 import com.dap_studio.spring_boot_expert_library.dao.CheckoutRepository;
 import com.dap_studio.spring_boot_expert_library.dao.HistoryRepository;
+import com.dap_studio.spring_boot_expert_library.dao.PaymentRepository;
 import com.dap_studio.spring_boot_expert_library.entity.Book;
 import com.dap_studio.spring_boot_expert_library.entity.Checkout;
 import com.dap_studio.spring_boot_expert_library.entity.History;
+import com.dap_studio.spring_boot_expert_library.entity.Payment;
 import com.dap_studio.spring_boot_expert_library.response_models.ShelfCurrentLoansResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.*;
+
 @Service
 @Transactional
 public class BookService {
@@ -25,12 +29,14 @@ public class BookService {
     private final BookRepository bookRepository;
     private final CheckoutRepository checkoutRepository;
     private final HistoryRepository historyRepository;
+    private final PaymentRepository paymentRepository;
 
     public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository,
-                       HistoryRepository historyRepository) {
+                       HistoryRepository historyRepository, PaymentRepository paymentRepository) {
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
         this.historyRepository = historyRepository;
+        this.paymentRepository = paymentRepository;
     }
 
     public Book checkoutBook (String userEmail, Long bookId) throws Exception {
@@ -39,6 +45,37 @@ public class BookService {
 
         if (!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0) {
             throw new Exception("Book does not exist or already checked out by user");
+        }
+
+        List<Checkout> currentBooksCheckedOut = checkoutRepository.findBooksByUserEmail(userEmail);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        boolean bookNeedsToBeReturned = false;
+
+        for (Checkout checkout : currentBooksCheckedOut) {
+            Date d1 = sdf.parse(checkout.getReturnDate());
+            Date d2 = sdf.parse(LocalDate.now().toString());
+
+            TimeUnit time = DAYS;
+
+            double differenceInTime = time.convert(d1.getTime() - d2.getTime(), MILLISECONDS);
+
+            if (differenceInTime < 0) {
+                bookNeedsToBeReturned = true;
+                break;
+            }
+        }
+
+        Payment userPayment = paymentRepository.findByUserEmail(userEmail);
+        if ((userPayment != null && userPayment.getAmount() > 0) || (userPayment != null && bookNeedsToBeReturned)) {
+            throw new Exception("Outstanding fees exists or book needs to be returned");
+        }
+
+        if (userPayment == null) {
+            Payment payment = new Payment();
+            payment.setUserEmail(userEmail);
+            payment.setAmount(0.00);
+            paymentRepository.save(payment);
         }
 
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
@@ -93,8 +130,8 @@ public class BookService {
                 Date d1 = sdf.parse(checkout.get().getReturnDate());
                 Date d2 = sdf.parse(LocalDate.now().toString());
 
-                TimeUnit time = TimeUnit.DAYS;
-                long diffInDays = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+                TimeUnit time = DAYS;
+                long diffInDays = time.convert(d1.getTime() - d2.getTime(), MILLISECONDS);
 
                 shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) diffInDays));
             }
